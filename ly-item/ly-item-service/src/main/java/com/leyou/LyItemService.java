38762f7313cc361526d755a65c954fcf85860943
meiheW <1925257530@qq.com>
2020-02-02 23:11:45 +0800

    搭建基础服务

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -0,0 +1,17 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author meihewang
+ * @date 2020/02/02  22:12
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class, args);
+    }
+}

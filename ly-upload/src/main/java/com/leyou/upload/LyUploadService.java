cbefff3ced8e9a0283a783171a9854d0ec45ace7
meiheW <1925257530@qq.com>
2020-02-09 01:13:28 +0800

    图片上传至本地

---
+++ ly-upload/src/main/java/com/leyou/upload/LyUploadService.java
@@ -0,0 +1,17 @@
+package com.leyou.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author meihewang
+ * @date 2020/02/07  14:38
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyUploadService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyUploadService.class, args);
+    }
+}
